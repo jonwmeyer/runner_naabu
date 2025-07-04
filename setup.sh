68b695dfef7c8be88438ff74507b885174e3549d
sungrazer <jonwmeyer@gmail.com>
2025-07-03 23:41:20 -0600

    .

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
     apt update
     apt install -y build-essential ca-certificates wget unzip git
     apt install -y python3 python3-pip python-is-python3
-    wget https://go.dev/dl/go1.21.6.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.21.6.linux-amd64.tar.gz && rm go1.21.6.linux-amd64.tar.gz
+    wget https://go.dev/dl/go1.24.4.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.24.4.linux-amd64.tar.gz && rm go1.21.6.linux-amd64.tar.gz
     export GOROOT=/usr/local/go
     export GOPATH=/go
     export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
